89d2f56aa4d558a312c10033097535b80eafb950
Matthew Giem and Stephanie Gurung <matthew+stephanie@>
2016-08-15 10:47:46 -0700

    update the JSON file so it includes announcments and links them to the announcements page

--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -1,15 +1,7 @@
 import Ember from 'ember';
 
-var announcements = [{
-  message: "There are free shish kebabs available at Stephanie's Rental today!",
-  date: "August 15"
-},
-{
-  message: "There's a fire at Stephanie's property from the kebab grill! Run away!",
-  date: "August 15"
-}];
 export default Ember.Route.extend({
   model() {
-    return announcements;
+    return this.store.findAll('announcement');
   }
 });
