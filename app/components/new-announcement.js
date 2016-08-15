8be598d1d49bad677a54b9427a4071a2e953f269
Matthew Giem and Stephanie Gurung <matthew@>
2016-08-15 11:25:16 -0700

    allow the user to add a new announcement to the announcements list

--- app/components/new-announcement.js
+++ app/components/new-announcement.js
@@ -5,6 +5,14 @@ export default Ember.Component.extend({
   actions:  {
     announcementFormShow()  {
       this.set('addNewAnnouncement', true)
+    },
+    save() {
+      var params = {
+        message: this.get('message'),
+        date: this.get('date') ? this.get('date'): "Future"
+      };
+      this.set('addNewAnnouncement', false);
+      this.sendAction('save', params);
     }
   }
 });
--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -8,6 +8,11 @@ export default Ember.Route.extend({
     destroyAnnouncement(announcement) {
       announcement.destroyRecord();
       this.transitionTo('announcements');
+    },
+    save(params) {
+      var newAnnouncement = this.store.createRecord('announcement', params);
+      newAnnouncement.save();
+      this.transitionTo('announcements');
     }
   }
 });
