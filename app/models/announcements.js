035ce8a00d2b141bd1b2b75ed7c0963edbb00e2d
Matthew Giem and Stephanie Gurung <matthew+stephanie@>
2016-08-15 10:33:44 -0700

    lists hard coded announcements on announcements page

--- app/models/announcements.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  message: DS.attr(),
-  date: DS.attr()
-});
--- app/routes/announcements.js
+++ app/routes/announcements.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
+var announcements = [{
+  message: "There are free shish kebabs available at Stephanie's Rental today!",
+  date: "August 15"
+},
+{
+  message: "There's a fire at Stephanie's property from the kebab grill! Run away!",
+  date: "August 15"
+}];
 export default Ember.Route.extend({
+  model() {
+    return announcements;
+  }
 });
