ece0d5a4cf0731accebd9354792e3825a24f1b37
Stephanie Gurung <stephr3@gmail.com>
2016-08-19 17:33:41 -0700

    add rental cost indicator on index route

--- app/components/new-rental.js
+++ app/components/new-rental.js
@@ -12,7 +12,8 @@ export default Ember.Component.extend({
         city: this.get('city'),
         type: this.get('type'),
         image: this.get('image') ? this.get('image'): "http://img06.deviantart.net/2031/i/2011/107/0/5/apartment_building___evening_by_dave5264-d3e7ol6.jpg",
-        bedrooms: this.get('bedrooms')
+        bedrooms: this.get('bedrooms'),
+        cost: parseInt(this.get('cost'))
       };
       this.set('addNewRental', false);
       this.sendAction('save2', params);
